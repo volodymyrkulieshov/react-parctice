c76ec5b41ad90059f9793e9d0ba08b575c7fc255
Volodymyr <vovkul@gmail.com>
2023-11-24 23:48:23 +0100

    ad practice part styles

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,10 +1,19 @@
 import data from "../../data.json";
 import css from "./Card.module.css";
+import clsx from "clsx";
 
 const Card = ({ children }) => {
   console.log("children :>> ", children);
+
+  // const styles = `${css.container} ${el.id % 2 === 0 ? "online" : "offline"}`;
   return data.map((el) => (
-    <div className={css.container} key={el.id}>
+    // <div className={css.container} key={el.id}>
+    // ============STYLES COLLECTION===========
+    <div
+      // className={clsx({ [css.container]: el.id % 2 === 0 })}
+      className={clsx(el.id % 2 === 0 ? "online" : "offline", css.container)}
+      key={el.id}
+    >
       {/* ========RENDER ПО УМОВІ========================= */}
       {/* {el.id % 2 === 0 ? <h1>OK</h1> : <h5>ERROR</h5>} */}
       {/* {el.id % 2 === 0 && <h1>OK</h1>} */}
