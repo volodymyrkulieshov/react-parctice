cf553483cbaa54b41461f643261ece4988c1540f
Volodymyr <vovkul@gmail.com>
2024-01-03 12:46:11 +0100

    Update ProductsList.jsx

--- src/components/ProductsList/ProductsList.jsx
+++ src/components/ProductsList/ProductsList.jsx
@@ -1,74 +1,129 @@
-import { Component } from "react";
-import Product from "./Product/Product";
-// import axios from "axios";
+// -------_USE HOOKS-----------------------////////
+import { useEffect, useState } from "react";
 import { getAllProducts } from "../../api/products";
+import Product from "./Product/Product";
 
-class ProductList extends Component {
-  state = {
-    products: null,
-    isLoading: false,
-    error: "",
-    isShowProducts: false,
-  };
+const ProductsList = () => {
+  const [products, setProducts] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+  const [isShowProducts, setIsShowProducts] = useState(false);
 
-  // componentDidMount() {
-  //   axios("https://dummyjson.com/products").then((response) => {
-  //     this.setState({ products: response.data.products });
-  //   });
-  // }
+  useEffect(() => {
+    getProducts();
+  }, [isShowProducts]);
 
-  componentDidMount() {
-    // this.getProducts();
-  }
-  componentDidUpdate(prevProps, prevState) {
-    if (
-      this.state.isShowProducts !== prevState.isShowProducts &&
-      this.state.isShowProducts
-    ) {
-      this.getProducts();
-    }
-    if (
-      this.state.isShowProducts !== prevState.isShowProducts &&
-      !this.state.isShowProducts
-    ) {
-      this.setState({ products: null });
-    }
-  }
-
-  getProducts = async () => {
+  const getProducts = async () => {
     try {
-      this.setState({ isLoading: true, error: "" });
+      // this.setState({ isLoading: true, error: "" });
+      setIsLoading(true);
+      setError("");
       const response = await getAllProducts();
-      this.setState({ products: response.products });
+      // this.setState({ products: response.products });
+      setProducts(response.products);
     } catch (error) {
-      console.log(error);
-      this.setState({ error: error.response.data });
+      // console.log(error);
+      // this.setState({ error: error.response.data });
       // this.setState({ error: error.message });
+      setError(error.message);
     } finally {
-      this.setState({ isLoading: false });
+      // this.setState({ isLoading: false });
+      setIsLoading(false);
     }
   };
-  handleClick = () => {
-    this.setState((prevState) => ({
-      isShowProducts: !prevState.isShowProducts,
-    }));
+  const handleClick = () => {
+    // this.setState((prevState) => ({
+    //   isShowProducts: !prevState.isShowProducts,
+    // }));
+    setIsShowProducts((prevState) => !isShowProducts);
   };
 
-  render() {
-    const { products, isLoading, error, isShowProducts } = this.state;
-    return (
-      <>
-        <button onClick={this.handleClick}>
-          {isShowProducts ? "Hide Products" : "Show products"}
-        </button>
-        {isLoading && <h1>Loading...</h1>}
-        {error && <h1>{error}</h1>}
-        {isShowProducts &&
-          products &&
-          products.map((elem) => <Product key={elem.id} product={elem} />)}
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <button onClick={handleClick}>
+        {isShowProducts ? "Hide Products" : "Show products"}
+      </button>
+      {isLoading && <h1>Loading...</h1>}
+      {error && <h1>{error}</h1>}
+      {isShowProducts &&
+        products &&
+        products.map((elem) => <Product key={elem.id} product={elem} />)}
+    </>
+  );
+};
 
-export default ProductList;
+export default ProductsList;
+// -------------USE CLASS COMPONENT--------------------/////////
+
+// import { Component, useState } from "react";
+// import Product from "./Product/Product";
+// // import axios from "axios";
+// import { getAllProducts } from "../../api/products";
+
+// class ProductList extends Component {
+//   state = {
+//     products: null,
+//     isLoading: false,
+//     error: "",
+//     isShowProducts: false,
+//   };
+
+//   // componentDidMount() {
+//   //   axios("https://dummyjson.com/products").then((response) => {
+//   //     this.setState({ products: response.data.products });
+//   //   });
+//   // }
+
+//   componentDidMount() {
+//     // this.getProducts();
+//   }
+//   componentDidUpdate(prevProps, prevState) {
+//     if (
+//       this.state.isShowProducts !== prevState.isShowProducts &&
+//       this.state.isShowProducts
+//     ) {
+//       this.getProducts();
+//     }
+//     if (
+//       this.state.isShowProducts !== prevState.isShowProducts &&
+//       !this.state.isShowProducts
+//     ) {
+//       this.setState({ products: null });
+//     }
+//   }
+
+//   getProducts = async () => {
+//     try {
+//       this.setState({ isLoading: true, error: "" });
+//       const response = await getAllProducts();
+//       this.setState({ products: response.products });
+//     } catch (error) {
+//       console.log(error);
+//       this.setState({ error: error.response.data });
+//       // this.setState({ error: error.message });
+//     } finally {
+//       this.setState({ isLoading: false });
+//     }
+//   };
+//   handleClick = () => {
+//     this.setState((prevState) => ({
+//       isShowProducts: !prevState.isShowProducts,
+//     }));
+//   };
+
+//   render() {
+//     const { products, isLoading, error, isShowProducts } = this.state;
+//     return (
+//       <>
+//         <button onClick={this.handleClick}>
+//           {isShowProducts ? "Hide Products" : "Show products"}
+//         </button>
+//         {isLoading && <h1>Loading...</h1>}
+//         {error && <h1>{error}</h1>}
+//         {isShowProducts &&
+//           products &&
+//           products.map((elem) => <Product key={elem.id} product={elem} />)}
+//       </>
+//     );
+//   }
+// }
