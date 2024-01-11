920b4bc0a717f06ef58e5512905599f360b92980
Volodymyr <vovkul@gmail.com>
2024-01-11 11:40:00 +0100

    add practice routes

--- src/components/ProductsList/Product/Product.jsx
+++ src/components/ProductsList/Product/Product.jsx
@@ -1,5 +1,32 @@
+// ----------МІЙ КОД------------//////\
+
+// import { Link } from "react-router-dom";
+
+// const Product = ({ product }) => {
+//   return (
+//     <Link to={`${product.id}`}>
+//       <div>{product.title}</div>;
+//     </Link>
+//   );
+// };
+
+// export default Product;
+
+// ---------- ROUTES (КОД ВИКЛАДАЧА)------------//////\
+
+import { Link } from "react-router-dom";
+
 const Product = ({ product }) => {
-  return <div>{product.title}</div>;
+  return (
+    <Link to={`${product.id}`}>
+      <div className="container">
+        <div className="card p-2">
+          <p>{product.title}</p>
+          <p>{product.price} $</p>
+        </div>
+      </div>
+    </Link>
+  );
 };
 
 export default Product;
